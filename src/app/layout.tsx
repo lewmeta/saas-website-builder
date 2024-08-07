d1d9a4e6117e0f5af3ad2b95bdd3e531ff04925a
thegypsyboyo <lewisonyango9@gmail.com>
2024-08-07 11:49:04 +0300

    made simple frontend site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from '@clerk/themes'
 
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'Plura',
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={font.className}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 
+import { pricingCards } from "@/lib/constants";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import clsx from "clsx";
+import { Check } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <section className="min-h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col">
-        {/* <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" /> */}
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
         <p className="text-center">Run your agency, in one place</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[300px]">
@@ -30,6 +37,72 @@ export default function Home() {
           ready to commit you can get started for free.
         </p>
       </section >
+      <section className="flex justify-center items-center flex-col gap-4 md:mt-20">
+        <h2 className="text-4xl text-center">
+          Choose what fits you right.
+        </h2>
+        <p className="text-muted-foreground text-center">
+          Our straightforward pricing plans are tailored to meet your needs. If
+          {" you're"} not <br />
+          ready to commit you can get started for free.
+        </p>
+        <div className="flex justify-center gap-4 flex-wrap mt-6">
+          {pricingCards.map((card) => (
+            <Card
+              key={card.priceId}
+              className={cn("w-[300px] flex flex-col justify-between", {
+                "": card.title === "Unlimited Saas",
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={clsx("", {
+                    "text-muted-foreground": card.title !== "Unlimited Saas",
+                  })}
+                >
+                  {card.title}
+                </CardTitle>
+                <CardDescription>
+                  {pricingCards.find((c) => c.title === card.title)?.description}
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">
+                  {card.price}
+                </span>
+                <span className="text-muted-foreground">
+                  <span>/m</span>
+                </span>
+              </CardContent>
+              <CardFooter className="flex flex-col items-start gap-4">
+                <div>
+                  {pricingCards.find((c) => c.title === card.title)?.features.map((feature) => (
+                    <div
+                      key={feature}
+                      className="flex gap-2"
+                    >
+                      <Check className="w-4 h-4" />
+                      <p>{feature}</p>
+                    </div>
+                  ))}
+                </div>
+                <Link
+                  href={`/agency?plan=${card.priceId}`}
+                  className={clsx(
+                    'w-full text-center bg-primary p-2 rounded-md',
+                    {
+                      '!bg-muted-foreground':
+                        card.title !== 'Unlimited Saas',
+                    }
+                  )}
+                >
+                  Get Started
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
